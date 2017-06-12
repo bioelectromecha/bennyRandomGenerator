1bb258bb4d53d8b0e5458fe832e56abb978fd12e
Roman Smirnov <bioelectromecha@gmail.com>
2017-06-12 13:08:53 +0300

    R.S - limited string length to 7 characters

--- main.py
+++ main.py
@@ -6,4 +6,6 @@ numbers = random.sample(range(1000,100000000), 100000)
 with open("Output.txt", "w") as text_file:
     for number in numbers:
         hex_string = '{:02x}'.format(number)
+        if len(hex_string)>7:
+            continue
         text_file.write(hex_string+"\n")
